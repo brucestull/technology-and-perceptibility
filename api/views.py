d911b55411e027bd1aef17cfaee91930937d1074
Bruce Stull <bruce.stull@gmail.com>
2022-04-29 19:25:06 -0400

    Changed permissions so non-owner's cannot remove TAPs.

--- api/views.py
+++ api/views.py
@@ -5,21 +5,22 @@ from django.contrib.auth import get_user_model
 
 from taps.models import Tap
 from api.serializers import TapSerializer, UserSerializer
+from api.permissions import IsAuthorOrReadOnly
 
 
 class TapViewSet(viewsets.ModelViewSet):
     queryset = Tap.objects.all()
     serializer_class = TapSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthorOrReadOnly]
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthorOrReadOnly]
 
 
-class PublicViewSet(viewsets.ModelViewSet):
+class PublicViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tap.objects.filter(public=True)
     serializer_class = TapSerializer
 
