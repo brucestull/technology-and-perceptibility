6f76e6d00db3f0f5a56e66902318fa75b1c1e147
Bruce Stull <bruce.stull@gmail.com>
2022-04-29 18:54:57 -0400

    Anonymous users now can click a link to view 'public' TAPs.

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,9 @@ from django.contrib.auth import get_user_model
 
 from taps.models import Tap
 
+
 class NestedTapSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Tap
         fields = (
@@ -14,19 +16,54 @@ class NestedTapSerializer(serializers.ModelSerializer):
                 'public',
                 )
 
+
 class NestedUserSerializer(serializers.ModelSerializer):
+
     class Meta: 
         model = get_user_model()
-        fields = ('id', 'username')
+        fields = (
+            'id',
+            'username'
+        )
+
 
 class TapSerializer(serializers.ModelSerializer):
-    author_detail = NestedUserSerializer(read_only=True, source='author')
+
+    author_detail = NestedUserSerializer(
+        read_only=True,
+        source='author'
+    )
+
     class Meta:
         model = Tap
-        fields = ('id', 'author', 'author_detail', 'url', 'url_label', 'description', 'public')
+        fields = (
+            'id',
+            'author',
+            'author_detail',
+            'url',
+            'url_label',
+            'description',
+            'public'
+        )
+
 
 class UserSerializer(serializers.ModelSerializer):
-    taps_detail = NestedTapSerializer(many=True, read_only=True, source='taps')
+
+    taps_detail = NestedTapSerializer(
+        many=True,
+        read_only=True,
+        source='taps'
+    )
+    taps_count = serializers.SerializerMethodField()
+
     class Meta: 
         model = get_user_model()
-        fields = ('id', 'username', 'taps_detail')
+        fields = (
+            'id',
+            'username',
+            'taps_count',
+            'taps_detail'
+        )
+
+    def get_taps_count(self, obj):
+        return Tap.objects.all().filter(author=obj.id).count()
