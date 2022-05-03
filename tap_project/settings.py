a02811fbaac816e0b63cfd9d911dcc3bda0da1e8
Bruce Stull <bruce.stull@gmail.com>
2022-05-03 10:41:13 -0400

    Cleaned up code, removed comments.

--- tap_project/settings.py
+++ tap_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-# from .secrets import DJANGO_KEY
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = DJANGO_KEY
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
